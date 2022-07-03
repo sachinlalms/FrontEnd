05f017e62bee07903e3382882fd51859b305fc8e
sachinlalms <sachinlalms95@gmail.com>
2022-07-03 23:04:45 +0530

    Pepsi_LandingPage

---
+++ Pepsi_landing/script.js
@@ -0,0 +1,7 @@
+function imgSlider(imagesd) {
+  document.querySelector(".pepsi").src = imagesd;
+}
+function changeBgColor(color) {
+  const sec = document.querySelector(".sec");
+  sec.style.background = color;
+}
